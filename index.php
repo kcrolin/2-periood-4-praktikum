18d7b54c090edfb16b89aa1dcac8ff5a4142c5b7
kcrolin <carolin.karing@khk.ee>
2014-10-16 11:41:04 +0100

    finish
    "vormis esines vigu"

--- index.php
+++ index.php
@@ -1,10 +1,12 @@
 <?php if (isset($_POST["submit"])) {
 $username = $_POST['username'];
 $password = $_POST['password'];
-$message = "Vormis esines vigu!"; }
+$message = "Vormis esines vigu!"; 
+echo "<style>.passw{border:1px solid red;}</style>";}
 else { 
 $message = "Palun logi sisse!";}
 ?>
+
 <!DOCTYPE HTML>
 <html>
   <head>
@@ -20,7 +22,7 @@ $message = "Palun logi sisse!";}
 <span>Kasutajanimi:</span> <input type="text" name="username" value="<?php if (isset($_POST["submit"])) {
 $username = $_POST['username'];
 echo $username;}?>"><br>
-<span>Salasõna:</span> <input type="password" name="password"></br>
+<span>Salasõna:</span> <input type="password" class="passw" name="password"></br>
 <input type="submit" name="submit">
 </form> 
 <pre><?php print_r($_POST);?></pre>
